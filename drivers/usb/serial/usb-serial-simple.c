e88b5202dcef7283430ae59420a5b9475eb0d140
Max Schulze <max.schulze@posteo.de>
2019-01-07 08:31:49 +0100

    USB: serial: simple: add Motorola Tetra TPG2200 device id
    commit b81c2c33eab79dfd3650293b2227ee5c6036585c upstream.
    
    Add new Motorola Tetra device id for Motorola Solutions TETRA PEI device
    
    T:  Bus=02 Lev=01 Prnt=01 Port=01 Cnt=01 Dev#=  4 Spd=480 MxCh= 0
    D:  Ver= 2.00 Cls=00(>ifc ) Sub=00 Prot=00 MxPS=64 #Cfgs=  1
    P:  Vendor=0cad ProdID=9016 Rev=24.16
    S:  Manufacturer=Motorola Solutions, Inc.
    S:  Product=TETRA PEI interface
    C:  #Ifs= 2 Cfg#= 1 Atr=80 MxPwr=500mA
    I:  If#= 0 Alt= 0 #EPs= 2 Cls=ff(vend.) Sub=00 Prot=00 Driver=usb_serial_simple
    I:  If#= 1 Alt= 0 #EPs= 2 Cls=ff(vend.) Sub=00 Prot=00 Driver=usb_serial_simple
    
    Signed-off-by: Max Schulze <max.schulze@posteo.de>
    Cc: stable <stable@vger.kernel.org>
    Signed-off-by: Johan Hovold <johan@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/serial/usb-serial-simple.c
+++ drivers/usb/serial/usb-serial-simple.c
@@ -20,99 +20,127 @@
 #include <linux/usb.h>
 #include <linux/usb/serial.h>
 
-#define DEVICE_N(vendor, IDS, nport)				\
-static const struct usb_device_id vendor##_id_table[] = {	\
-	IDS(),							\
-	{ },							\
-};								\
-static struct usb_serial_driver vendor##_device = {		\
-	.driver = {						\
-		.owner =	THIS_MODULE,			\
-		.name =		#vendor,			\
-	},							\
-	.id_table =		vendor##_id_table,		\
-	.num_ports =		nport,				\
-};
+#define DEVICE_N(vendor, IDS, nport)                          \
+	static const struct usb_device_id vendor##_id_table[] = { \
+		IDS(),                                                \
+		{},                                                   \
+	};                                                        \
+	static struct usb_serial_driver vendor##_device = {       \
+		.driver = {                                           \
+			.owner = THIS_MODULE,                             \
+			.name = #vendor,                                  \
+		},                                                    \
+		.id_table = vendor##_id_table,                        \
+		.num_ports = nport,                                   \
+	};
 
-#define DEVICE(vendor, IDS)	DEVICE_N(vendor, IDS, 1)
+#define DEVICE(vendor, IDS) DEVICE_N(vendor, IDS, 1)
 
 /* Medtronic CareLink USB driver */
-#define CARELINK_IDS()			\
-	{ USB_DEVICE(0x0a21, 0x8001) }	/* MMT-7305WW */
+#define CARELINK_IDS()             \
+	{                              \
+		USB_DEVICE(0x0a21, 0x8001) \
+	} /* MMT-7305WW */
 DEVICE(carelink, CARELINK_IDS);
 
 /* ZIO Motherboard USB driver */
-#define ZIO_IDS()			\
-	{ USB_DEVICE(0x1CBE, 0x0103) }
+#define ZIO_IDS()                  \
+	{                              \
+		USB_DEVICE(0x1CBE, 0x0103) \
+	}
 DEVICE(zio, ZIO_IDS);
 
 /* Funsoft Serial USB driver */
-#define FUNSOFT_IDS()			\
-	{ USB_DEVICE(0x1404, 0xcddc) }
+#define FUNSOFT_IDS()              \
+	{                              \
+		USB_DEVICE(0x1404, 0xcddc) \
+	}
 DEVICE(funsoft, FUNSOFT_IDS);
 
 /* Infineon Flashloader driver */
-#define FLASHLOADER_IDS()		\
-	{ USB_DEVICE_INTERFACE_CLASS(0x058b, 0x0041, USB_CLASS_CDC_DATA) }, \
-	{ USB_DEVICE(0x8087, 0x0716) }, \
-	{ USB_DEVICE(0x8087, 0x0801) }
+#define FLASHLOADER_IDS()                                             \
+	{USB_DEVICE_INTERFACE_CLASS(0x058b, 0x0041, USB_CLASS_CDC_DATA)}, \
+		{USB_DEVICE(0x8087, 0x0716)},                                 \
+	{                                                                 \
+		USB_DEVICE(0x8087, 0x0801)                                    \
+	}
 DEVICE(flashloader, FLASHLOADER_IDS);
 
 /* Google Serial USB SubClass */
-#define GOOGLE_IDS()						\
-	{ USB_VENDOR_AND_INTERFACE_INFO(0x18d1,			\
-					USB_CLASS_VENDOR_SPEC,	\
-					0x50,			\
-					0x01) }
+#define GOOGLE_IDS()                                         \
+	{                                                        \
+		USB_VENDOR_AND_INTERFACE_INFO(0x18d1,                \
+									  USB_CLASS_VENDOR_SPEC, \
+									  0x50,                  \
+									  0x01)                  \
+	}
 DEVICE(google, GOOGLE_IDS);
 
 /* Libtransistor USB console */
-#define LIBTRANSISTOR_IDS()			\
-	{ USB_DEVICE(0x1209, 0x8b00) }
+#define LIBTRANSISTOR_IDS()        \
+	{                              \
+		USB_DEVICE(0x1209, 0x8b00) \
+	}
 DEVICE(libtransistor, LIBTRANSISTOR_IDS);
 
 /* ViVOpay USB Serial Driver */
-#define VIVOPAY_IDS()			\
-	{ USB_DEVICE(0x1d5f, 0x1004) }	/* ViVOpay 8800 */
+#define VIVOPAY_IDS()              \
+	{                              \
+		USB_DEVICE(0x1d5f, 0x1004) \
+	} /* ViVOpay 8800 */
 DEVICE(vivopay, VIVOPAY_IDS);
 
 /* Motorola USB Phone driver */
-#define MOTO_IDS()			\
-	{ USB_DEVICE(0x05c6, 0x3197) },	/* unknown Motorola phone */	\
-	{ USB_DEVICE(0x0c44, 0x0022) },	/* unknown Motorola phone */	\
-	{ USB_DEVICE(0x22b8, 0x2a64) },	/* Motorola KRZR K1m */		\
-	{ USB_DEVICE(0x22b8, 0x2c84) },	/* Motorola VE240 phone */	\
-	{ USB_DEVICE(0x22b8, 0x2c64) }	/* Motorola V950 phone */
+#define MOTO_IDS()                                                 \
+	{USB_DEVICE(0x05c6, 0x3197)},	  /* unknown Motorola phone */ \
+		{USB_DEVICE(0x0c44, 0x0022)}, /* unknown Motorola phone */ \
+		{USB_DEVICE(0x22b8, 0x2a64)}, /* Motorola KRZR K1m */      \
+		{USB_DEVICE(0x22b8, 0x2c84)}, /* Motorola VE240 phone */   \
+	{                                                              \
+		USB_DEVICE(0x22b8, 0x2c64)                                 \
+	} /* Motorola V950 phone */
 DEVICE(moto_modem, MOTO_IDS);
 
 /* Motorola Tetra driver */
-#define MOTOROLA_TETRA_IDS()			\
-	{ USB_DEVICE(0x0cad, 0x9011) }	/* Motorola Solutions TETRA PEI */
+#define MOTOROLA_TETRA_IDS()                                             \
+	{USB_DEVICE(0x0cad, 0x9011)},	  /* Motorola Solutions TETRA PEI */ \
+		{USB_DEVICE(0x0cad, 0x9012)}, /* MTP6550 */                      \
+	{                                                                    \
+		USB_DEVICE(0x0cad, 0x9016)                                       \
+	} /* TPG2200 */
 DEVICE(motorola_tetra, MOTOROLA_TETRA_IDS);
 
 /* Novatel Wireless GPS driver */
-#define NOVATEL_IDS()			\
-	{ USB_DEVICE(0x09d7, 0x0100) }	/* NovAtel FlexPack GPS */
+#define NOVATEL_IDS()              \
+	{                              \
+		USB_DEVICE(0x09d7, 0x0100) \
+	} /* NovAtel FlexPack GPS */
 DEVICE_N(novatel_gps, NOVATEL_IDS, 3);
 
 /* HP4x (48/49) Generic Serial driver */
-#define HP4X_IDS()			\
-	{ USB_DEVICE(0x03f0, 0x0121) }
+#define HP4X_IDS()                 \
+	{                              \
+		USB_DEVICE(0x03f0, 0x0121) \
+	}
 DEVICE(hp4x, HP4X_IDS);
 
 /* Suunto ANT+ USB Driver */
-#define SUUNTO_IDS()			\
-	{ USB_DEVICE(0x0fcf, 0x1008) },	\
-	{ USB_DEVICE(0x0fcf, 0x1009) } /* Dynastream ANT USB-m Stick */
+#define SUUNTO_IDS()               \
+	{USB_DEVICE(0x0fcf, 0x1008)},  \
+	{                              \
+		USB_DEVICE(0x0fcf, 0x1009) \
+	} /* Dynastream ANT USB-m Stick */
 DEVICE(suunto, SUUNTO_IDS);
 
 /* Siemens USB/MPI adapter */
-#define SIEMENS_IDS()			\
-	{ USB_DEVICE(0x908, 0x0004) }
+#define SIEMENS_IDS()             \
+	{                             \
+		USB_DEVICE(0x908, 0x0004) \
+	}
 DEVICE(siemens_mpi, SIEMENS_IDS);
 
 /* All of the above structures mushed into two lists */
-static struct usb_serial_driver * const serial_drivers[] = {
+static struct usb_serial_driver *const serial_drivers[] = {
 	&carelink_device,
 	&zio_device,
 	&funsoft_device,
@@ -126,8 +154,7 @@ static struct usb_serial_driver * const serial_drivers[] = {
 	&hp4x_device,
 	&suunto_device,
 	&siemens_mpi_device,
-	NULL
-};
+	NULL};
 
 static const struct usb_device_id id_table[] = {
 	CARELINK_IDS(),
@@ -143,7 +170,7 @@ static const struct usb_device_id id_table[] = {
 	HP4X_IDS(),
 	SUUNTO_IDS(),
 	SIEMENS_IDS(),
-	{ },
+	{},
 };
 MODULE_DEVICE_TABLE(usb, id_table);
 
